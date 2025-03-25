86cd8f3714c9b830e418b905b825c103b537ff2a
pedroernesto <pedro.garcia-rodriguez@univ-amu.fr>
2025-03-25 16:18:25 +0100

    Creating Python module for input functions

--- abi_dcm/utils/functions_plot.py
+++ abi_dcm/utils/functions_plot.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
-import numpy as np
+import seaborn as sns
+from numpy import NaN
+import jax.numpy as jnp
 
 def plot_signals(time_pts=None, data_plot=None, num_plots=5, label_= 'Signal'):
 
@@ -67,3 +69,33 @@ def plot_correlations(trial=0, ROI=0, data=None):
     # plt.gca().set_box_aspect(0.7)
     axes[1].set_xlabel('lag')
     axes[1].set_xlim(-0.5, lag_max)
+    
+
+def plot_matrix(A=None, title_str='A', title_size=20, cmap='viridis', no_diag=True, no_zeros= True, \
+                annot=True, annot_fs=8, annot_fmt=".3f", \
+                shrink=1.0, vmin=None, vmax=None, cbar=False, ytickls=False, xtickls=False):
+    '''Plots matrix A as a seaborn heatmap'''
+    
+    if no_diag:
+        diag_idx = jnp.diag_indices_from(A)
+        diag_A = A[0,0]
+        A = A.at[diag_idx].set(NaN)
+    if no_zeros:
+        A = A.at[jnp.where(A==0.00)].set(NaN)
+    
+    kw = dict(cmap=cmap, vmin=vmin, vmax=vmax, square=True, cbar_kws={"shrink": shrink}, annot_kws={"fontsize":annot_fs})
+    ax = sns.heatmap(A, **kw, cbar=cbar, yticklabels=ytickls, xticklabels=xtickls, annot=annot, fmt=annot_fmt)
+    if no_diag and ~jnp.isnan(diag_A): 
+        for i in range(len(A)):
+            ax.text(diag_idx[0][i]+.5, diag_idx[1][i]+.5, f'{diag_A: .3f}', fontsize=annot_fs, \
+                    horizontalalignment='center', verticalalignment='center', )
+    ax.set_yticklabels(ax.get_yticklabels(),rotation=30)
+    ax.set_xticklabels(ax.get_xticklabels(),rotation=70)
+
+    # Drawing the frame 
+    for _, spine in ax.spines.items():
+        spine.set_visible(True)
+        spine.set_linewidth(1) 
+    
+    plt.title(title_str, fontsize=title_size)
+    
---
+++ abi_dcm/utils/functions_stims.py
@@ -0,0 +1,47 @@
+import jax, jax.numpy as jnp
+
+def stim_time_shape(shape=None, stim_onset=None, ntime=None, ninputs=1, \
+                    stim_tau=None, alpha=None, beta=None):
+    '''Computes a signal stimulus with a specific shape'''
+    ts_stim = jnp.tile(jnp.r_[:ntime-stim_onset],(ninputs,1))
+    match shape:
+        case 'Exp':   #Exponential shape        
+            stim = jnp.exp(-ts_stim/stim_tau)
+    
+        case 'Alpha': #Alpha shape
+            # Normalized as a probability distribution
+            # mean = 2*stim_tau, var = 2*stim_tau**2, mode = stim_tau
+            stim = ts_stim/stim_tau**2*jnp.exp(-ts_stim/stim_tau)
+            # stim = ts_stim/stim_tau*jnp.exp(1-ts_stim/stim_tau)
+    
+        case 'Gamma': # Gamma shape (alpha>1)
+            # mean = theta_mu = alpha/beta, var = theta_sigma = mean/beta
+            # mode = (alpha-1)/beta = (theta_mu**2-theta_sigma)/theta_mu
+            stim = jnp.power(beta,alpha)*jnp.power(ts_stim,alpha-1) \
+                  *jnp.exp(-beta*ts_stim)/jax.scipy.special.gamma(alpha)
+            
+        case 'Pulse': #Pulse shape
+            stim = jnp.ones((dur_dt,))
+        
+        case _:
+            print('Signal shape not recognized')
+
+    return stim
+
+
+def stim_time_full(stim_shape, stim_onset=None, ntime=None, ninputs=1):
+    '''Computes a signal stimulus in the full time domain'''
+    stim_time = jnp.zeros((ntime,))    
+    return jax.lax.dynamic_update_slice(stim_time, stim_shape, (stim_onset,))
+
+
+def stim_signal(shape='Alpha', ntrl=1, ninputs=1, stim_onset=None, ntime=None, \
+                stim_tau=None, alpha=None, beta=None):
+    '''Constructs a set of different stimulus signals, but identical across trials'''
+    INPUTs = jnp.r_[:ninputs]
+    stim_shape = stim_time_shape(shape, stim_onset, ntime, ninputs, stim_tau, alpha, beta)
+    
+    stim_trl = jax.vmap(lambda input: stim_time_full(stim_shape[input], stim_onset, ntime))(INPUTs)
+    stim_trl = jnp.expand_dims(stim_trl,axis=0)
+    
+    return jnp.tile(stim_trl, (ntrl,1,1))
